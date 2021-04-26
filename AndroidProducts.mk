cfcfd42b3a6c357fdac5822f1a0aa4202e8f2aab
Your Name <you@example.com>
2021-04-26 20:34:13 +0000

    wave os changes

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/wave_gauguin.mk
+
+COMMON_LUNCH_CHOICES := \
+    wave_gauguin-user \
+    wave_gauguin-userdebug \
+    wave_gauguin-eng
---
+++ wave_gauguin.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from gauguin device
+$(call inherit-product, device/xiaomi/gauguin/device.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common WaveOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/wave/configs/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := wave_gauguin
+PRODUCT_DEVICE := gauguin
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

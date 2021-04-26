ed63e9fe2bd7110833119d834519c95d96c1d44f
Badavath Tarun <tarunofman@gmail.com>
2021-04-26 10:54:14 +0530

    some changes here and there and smart charge i guess

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/octavi_gauguin.mk
+
+COMMON_LUNCH_CHOICES := \
+    octavi_gauguin-user \
+    octavi_gauguin-userdebug \
+    octavi_gauguin-eng
